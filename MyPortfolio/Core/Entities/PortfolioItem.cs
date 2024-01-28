d07017b4e8f971c4c66ff5222be8baea9c4e21a5
farouk <faroukola99@gmail.com>
2024-01-28 16:17:04 +0200

    Added my project

---
+++ MyPortfolio/Core/Entities/PortfolioItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class PortfolioItem : EntityBase
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+
+        [Display(Name = "Picture")]
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ MyPortfolio/Web/Models/PortfolioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class PortfolioViewModel
+    {
+        public Guid Id { get; set; }
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
